7b0787c3ea29a5cc7cb7ac5427a8194b8cbdc52a
ancf-hue <an.cf@yandex.ru>
2026-04-21 22:24:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'anti-ai-level-v1.4';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => { const c = resp.clone(); caches.open(CACHE).then(cache => cache.put(e.request, c)); return resp; }).catch(() => caches.match('./index.html'))));
+});
